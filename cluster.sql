b295bf7cc1beb8e8a96699b6b25be9e3cfcd4bcd
Sinol95 <la.laguet95@gmail.com>
2024-03-19 14:02:38 +0100

    Cluster and index refacto

---
+++ cluster.sql
@@ -0,0 +1,6 @@
+-- Création d'un cluster sur la colonne location de la table glpi_tickets
+-- Les clusters regroupent physiquement les lignes de données en fonction des valeurs des colonnes de regroupement spécifiées
+CREATE CLUSTER clst_glpi_tickets_location (location) 
+    SINGLE TABLESPACE admin 
+    SIZE 1024M
+    HASHKEYS 100;
---
+++ index.sql
@@ -0,0 +1,29 @@
+-- Création d'un index sur la colonne location de la table glpi_tickets
+-- Cet index accélérera les opérations de recherche basées sur la localisation des tickets
+CREATE INDEX idx_glpi_tickets_location ON glpi_tickets(location);
+
+-- Création d'un index sur la colonne glpi_tickets_category_id de la table glpi_tickets
+-- Cet index améliorera les performances des requêtes impliquant la catégorie des tickets
+CREATE INDEX idx_glpi_tickets_category_id ON glpi_tickets(glpi_tickets_category_id);
+
+-- Création d'un index sur la colonne entites_id de la table glpi_tickets
+-- Cet index améliorera les performances des requêtes impliquant les entités des tickets
+CREATE INDEX idx_glpi_tickets_entities_id ON glpi_tickets(entites_id);
+
+-- Création d'un index sur la colonne entites_id de la table glpi_treated_tickets
+-- Cet index améliorera les performances des requêtes impliquant les entités des tickets traités
+CREATE INDEX idx_glpi_treated_tickets_entities_id ON glpi_treated_tickets(entites_id);
+
+-- Création d'un index sur la colonne email de la table glpi_users
+-- Cet index améliorera les performances des requêtes impliquant l'email des utilisateurs
+CREATE INDEX idx_glpi_users_email ON glpi_users(email);
+
+-- Création d'un index sur la colonne user_id_1 de la table glpi_notifications
+-- Cet index améliorera les performances des requêtes impliquant l'ID de l'utilisateur 1 dans les notifications
+CREATE INDEX idx_glpi_notifications_user_id_1 ON glpi_notifications(user_id_1);
+
+-- Création d'un index sur la colonne user_id_2 de la table glpi_notifications
+-- Cet index améliorera les performances des requêtes impliquant l'ID de l'utilisateur 2 dans les notifications
+CREATE INDEX idx_glpi_notifications_user_id_2 ON glpi_notifications(user_id_2);
+
+COMMIT;
