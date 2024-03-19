f33221e68bf70d3af37e18853c951cefeb041c83
Sinol95 <la.laguet95@gmail.com>
2024-03-19 14:29:10 +0100

    procedure : solveDate to wasSolved

--- procedure.sql
+++ procedure.sql
@@ -1,11 +1,11 @@
 -- Procédure pour clore un ticket
+-- Cette procédure clôture un ticket en le déplaçant vers la table des tickets traités
 CREATE OR REPLACE PROCEDURE close_ticket (
     p_ticket_id IN NUMBER
 ) AS
 BEGIN
     -- Insérer dans la table glpi_treated_tickets en fonction des données du ticket existant
-    -- Cette procédure clôture un ticket en le déplaçant vers la table des tickets traités
-    INSERT INTO glpi_treated_tickets (id, ticket_id, entites_id, name, date, closedDate, solveDate, glpi_tickets_category_id, location, items_id)
+    INSERT INTO glpi_treated_tickets (id, ticket_id, entites_id, name, date, closedDate, wasSolved, glpi_tickets_category_id, location, items_id)
     SELECT
         glpi_treated_tickets_seq.NEXTVAL,
         id,
@@ -14,6 +14,7 @@ BEGIN
         date,
         SYSTIMESTAMP,
         CASE WHEN USER IN (SELECT GRANTED_ROLE FROM DBA_ROLE_PRIVS WHERE GRANTEE = USER AND GRANTED_ROLE = 'ADMIN_ROLE') THEN SYSTIMESTAMP ELSE NULL END,
+        TRUE, -- Le ticket est considéré comme résolu lorsqu'il est clos
         glpi_tickets_category_id,
         location,
         items_id
@@ -30,12 +31,12 @@ END;
 /
 
 -- Procédure pour rouvrir un ticket fermé
+-- Cette procédure rouvre un ticket en le déplaçant de la table des tickets traités à la table des tickets ouverts
 CREATE OR REPLACE PROCEDURE reopen_closed_ticket (
     p_ticket_id IN NUMBER
 ) AS
 BEGIN
     -- Insérer dans la table glpi_tickets en fonction des données du ticket traité existant
-    -- Cette procédure rouvre un ticket en le déplaçant de la table des tickets traités à la table des tickets ouverts
     INSERT INTO glpi_tickets (id, entites_id, name, date, user_id_last_updater, glpi_tickets_category_id, status, location, items_id)
     SELECT
         ticket_id,
@@ -60,6 +61,7 @@ END;
 /
 
 -- Procédure pour créer un nouvel utilisateur
+-- Cette procédure crée un nouvel utilisateur dans le système avec un rôle par défaut.
 CREATE OR REPLACE PROCEDURE create_user_procedure (
     p_username IN VARCHAR2,
     p_password IN VARCHAR2
@@ -75,6 +77,7 @@ END create_user_procedure;
 /
 
 -- Procédure pour ajouter un administrateur avec un rôle spécifique basé sur son emplacement
+-- Cette procédure attribue un rôle spécifique à un utilisateur en fonction de son emplacement.
 CREATE OR REPLACE PROCEDURE add_admin_procedure (
     p_user_id IN NUMBER,
     p_location IN VARCHAR2
