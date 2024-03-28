7f1f087f4059905f2369e18f3a6024816b15af67
Sinol95 <la.laguet95@gmail.com>
2024-03-28 11:06:29 +0100

    Indentation du code lié aux vues

--- Cergy/views_cergy.sql
+++ Cergy/views_cergy.sql
@@ -1,9 +1,20 @@
-CREATE OR REPLACE VIEW selectTicketFromFermat AS SELECT * FROM glpi_tickets WHERE location = 'Fermat';
-CREATE OR REPLACE VIEW selectTicketFromCondorcet AS SELECT * FROM glpi_tickets WHERE location = 'Condorcet';
-CREATE OR REPLACE VIEW selectTicketFromTuring AS SELECT * FROM glpi_tickets WHERE location = 'Turing';
-CREATE OR REPLACE VIEW selectTicketFromCauchy AS SELECT * FROM glpi_tickets WHERE location = 'Cauchy';
-CREATE OR REPLACE VIEW selectTicketFromMartin AS SELECT * FROM glpi_tickets WHERE location = 'Saint Martin';
-CREATE OR REPLACE VIEW selectTicketFromGermain AS SELECT * FROM glpi_tickets WHERE location = 'Saint Germain';
+CREATE OR REPLACE VIEW selectTicketFromFermat AS 
+SELECT * FROM glpi_tickets WHERE location = 'Fermat';
+
+CREATE OR REPLACE VIEW selectTicketFromCondorcet AS 
+SELECT * FROM glpi_tickets WHERE location = 'Condorcet';
+
+CREATE OR REPLACE VIEW selectTicketFromTuring AS 
+SELECT * FROM glpi_tickets WHERE location = 'Turing';
+
+CREATE OR REPLACE VIEW selectTicketFromCauchy AS 
+SELECT * FROM glpi_tickets WHERE location = 'Cauchy';
+
+CREATE OR REPLACE VIEW selectTicketFromMartin AS 
+SELECT * FROM glpi_tickets WHERE location = 'Saint Martin';
+
+CREATE OR REPLACE VIEW selectTicketFromGermain AS
+SELECT * FROM glpi_tickets WHERE location = 'Saint Germain';
 
 GRANT INSERT, DELETE, SELECT, UPDATE ON selectTicketFromFermat TO technician_role;
 GRANT INSERT, DELETE, SELECT, UPDATE ON selectTicketFromCondorcet TO technician_role;
--- view.sql
+++ view.sql
@@ -4,8 +4,22 @@ DROP DATABASE LINK link_glpiAdmin_glpiAdmin_pau;
 CREATE DATABASE LINK link_glpiAdmin_glpiAdmin_cergy CONNECT TO glpiAdmin_cergy IDENTIFIED BY cergy USING '127.0.0.1:1521';
 CREATE DATABASE LINK link_glpiAdmin_glpiAdmin_pau CONNECT TO glpiAdmin_pau IDENTIFIED BY pau USING '127.0.0.1:1521';
 
-CREATE OR REPLACE VIEW selectAllTickets AS SELECT * FROM glpi_tickets@link_glpiAdmin_glpiAdmin_cergy UNION SELECT * FROM glpi_tickets@link_glpiAdmin_glpiAdmin_pau;
-CREATE OR REPLACE VIEW selectAllClosedTickets AS SELECT * FROM glpi_treated_tickets@link_glpiAdmin_glpiAdmin_cergy UNION SELECT * FROM glpi_treated_tickets@link_glpiAdmin_glpiAdmin_pau;
-CREATE OR REPLACE VIEW selectAllUsers AS SELECT * FROM glpi_users@link_glpiAdmin_glpiAdmin_cergy UNION SELECT * FROM glpi_users@link_glpiAdmin_glpiAdmin_pau;
+CREATE OR REPLACE VIEW selectAllTickets AS 
+SELECT *
+FROM glpi_tickets@link_glpiAdmin_glpiAdmin_cergy
+UNION
+SELECT * FROM glpi_tickets@link_glpiAdmin_glpiAdmin_pau;
+
+CREATE OR REPLACE VIEW selectAllClosedTickets AS 
+SELECT * FROM glpi_treated_tickets@link_glpiAdmin_glpiAdmin_cergy 
+UNION 
+SELECT * 
+FROM glpi_treated_tickets@link_glpiAdmin_glpiAdmin_pau;
+
+CREATE OR REPLACE VIEW selectAllUsers AS
+SELECT *
+FROM glpi_users@link_glpiAdmin_glpiAdmin_cergy 
+UNION
+SELECT * FROM glpi_users@link_glpiAdmin_glpiAdmin_pau;
 
 COMMIT;
